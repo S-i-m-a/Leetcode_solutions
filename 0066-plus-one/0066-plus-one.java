014137bf66816e8e88b3ba5590acf92ba269272f
Sima Pandit <160927044+S-i-m-a@users.noreply.github.com>
2025-01-29 09:57:29 +0530

    Time: 0 ms (100%), Space: 42 MB (46.53%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,22 +1,15 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        int n = digits.length;
-        
-        // Traverse the array from the end to the beginning
-        for (int i = n - 1; i >= 0; i--) {
-            // If the current digit is less than 9, simply add one and return the array
+        for (int i = digits.length - 1; i >= 0; i--) {
             if (digits[i] < 9) {
                 digits[i]++;
                 return digits;
             }
-            // If the digit is 9, it becomes 0 and carry over to the next digit
             digits[i] = 0;
         }
-        
-        // If all digits were 9, we have a carry that adds a new digit at the beginning
-        int[] newNumber = new int[n + 1];
-        newNumber[0] = 1;
-        
-        return newNumber;
+
+        int[] result = new int[digits.length + 1];
+        result[0] = 1;
+        return result;
     }
 }
