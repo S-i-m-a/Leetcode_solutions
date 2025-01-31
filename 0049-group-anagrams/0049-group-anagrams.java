01e27a42084550ce8d142eaa6184b11ca87ef79d
Sima Pandit <160927044+S-i-m-a@users.noreply.github.com>
2025-01-31 08:36:19 +0530

    Time: 7 ms (64.83%), Space: 47.9 MB (68.67%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return new ArrayList<>();
+        }
+        
+        Map<String, List<String>> anagramMap = new HashMap<>();
+        
+        for (String str : strs) {
+            char[] charArray = str.toCharArray();
+            Arrays.sort(charArray);
+            String sortedStr = new String(charArray);
+            
+            anagramMap.computeIfAbsent(sortedStr, k -> new ArrayList<>()).add(str);
+        }
+        
+        return new ArrayList<>(anagramMap.values());
+    }
+}
