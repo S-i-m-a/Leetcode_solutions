8fe00f8282d23e58ea5956442e730e7aaa87ffdd
Sima Pandit <160927044+S-i-m-a@users.noreply.github.com>
2025-01-31 08:41:53 +0530

    Time: 4 ms (97.06%), Space: 45 MB (43.16%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -1,39 +1,35 @@
+import java.util.*;
+
 class MinStack {
-    // Main stack to store the elements
     private Stack<Integer> stack;
-    // Auxiliary stack to store the minimum elements
     private Stack<Integer> minStack;
 
     public MinStack() {
-        // Initialize the stacks
         stack = new Stack<>();
         minStack = new Stack<>();
     }
-    
-    public void push(int x) {
-        // Push the element to the main stack
-        stack.push(x);
-        // Push the minimum element to the min stack
-        if (minStack.isEmpty() || x <= minStack.peek()) {
-            minStack.push(x);
+
+    public void push(int val) {
+        stack.push(val);
+        if (minStack.isEmpty() || val <= minStack.peek()) {
+            minStack.push(val);
         }
     }
-    
+
     public void pop() {
-        // Pop from both stacks if the element to pop is the same as the minimum
-        if (stack.peek().equals(minStack.peek())) {
-            minStack.pop();
+        if (!stack.isEmpty()) {
+            if (stack.peek().equals(minStack.peek())) {
+                minStack.pop();
+            }
+            stack.pop();
         }
-        stack.pop();
     }
-    
+
     public int top() {
-        // Return the top element from the main stack
         return stack.peek();
     }
-    
+
     public int getMin() {
-        // Return the minimum element from the min stack
         return minStack.peek();
     }
 }
