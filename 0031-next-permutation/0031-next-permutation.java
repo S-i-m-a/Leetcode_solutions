f3afc0ae5de555957fec0b93857b42b98053674f
Sima Pandit <160927044+S-i-m-a@users.noreply.github.com>
2025-04-20 18:03:59 +0530

    Time: 0 ms (100%), Space: 43.2 MB (56.8%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -5,22 +5,20 @@ public class Solution {
         int n = nums.length;
         int i = n - 2;
 
-        // Step 1: Find the first decreasing element from the right
         while (i >= 0 && nums[i] >= nums[i + 1]) {
             i--;
         }
 
         if (i >= 0) {
-            // Step 2: Find the element just larger than nums[i]
+        
             int j = n - 1;
             while (nums[j] <= nums[i]) {
                 j--;
             }
-            // Step 3: Swap nums[i] and nums[j]
+            
             swap(nums, i, j);
         }
-
-        // Step 4: Reverse the subarray from i + 1 to the end
+        
         reverse(nums, i + 1, n - 1);
     }
 
