930469ae5ddce6a04bd0e6e0a3fb12adb53be81e
Sima Pandit <160927044+S-i-m-a@users.noreply.github.com>
2025-06-01 07:18:01 +0545

    Time: 6 ms (98.07%), Space: 48.1 MB (36.38%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,21 +1,24 @@
 import java.util.*;
 
-class Solution {
+public class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        if (strs == null || strs.length == 0) {
+        if (strs == null || strs.length == 0)
             return new ArrayList<>();
+
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String s : strs) {
+            char[] chars = s.toCharArray();
+            Arrays.sort(chars); // sort the characters in the string
+            String key = new String(chars); // sorted string is the key
+
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
+
+            map.get(key).add(s); // group anagrams by the sorted key
         }
-        
-        Map<String, List<String>> anagramMap = new HashMap<>();
-        
-        for (String str : strs) {
-            char[] charArray = str.toCharArray();
-            Arrays.sort(charArray);
-            String sortedStr = new String(charArray);
-            
-            anagramMap.computeIfAbsent(sortedStr, k -> new ArrayList<>()).add(str);
-        }
-        
-        return new ArrayList<>(anagramMap.values());
+
+        return new ArrayList<>(map.values());
     }
 }
