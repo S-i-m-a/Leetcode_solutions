31f01329ce3f56bf50de9956adf87dedd8b688d4
Sima Pandit <160927044+S-i-m-a@users.noreply.github.com>
2025-08-03 13:18:44 +0530

    Time: 0 ms (100%), Space: 40.5 MB (98.93%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -1,11 +1,16 @@
-class Solution {
+public class Solution {
     public int reverseBits(int n) {
         int result = 0;
+        
         for (int i = 0; i < 32; i++) {
+            // Shift result to the left to make space
             result <<= 1;
+            // Add the last bit of n to result
             result |= (n & 1);
-            n >>>= 1;
+            // Shift n to the right to process next bit
+            n >>= 1;
         }
+        
         return result;
     }
 }
