f1a879b0225aca71b8195231136ac36dd10a48d7
Sima Pandit <160927044+S-i-m-a@users.noreply.github.com>
2025-08-13 17:15:26 +0530

    Time: 8 ms (84.41%), Space: 44.5 MB (10.93%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.java
+++ 0326-power-of-three/0326-power-of-three.java
@@ -1,9 +1,9 @@
 class Solution {
     public boolean isPowerOfThree(int n) {
-        if (n <= 0) return false; // Power of three must be positive
+        if (n <= 0) return false;
         while (n % 3 == 0) {
-            n /= 3; // Divide n by 3
+            n /= 3;
         }
-        return n == 1; // Check if n reduced to 1
+        return n == 1;
     }
 }
