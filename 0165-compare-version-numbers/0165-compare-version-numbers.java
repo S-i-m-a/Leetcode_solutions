0499a5574ed53ce8eb12d13c35e6b3d794babfc8
Sima Pandit <160927044+S-i-m-a@users.noreply.github.com>
2025-09-23 13:40:42 +0530

    Time: 1 ms (76.03%), Space: 42 MB (6.38%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.java
+++ 0165-compare-version-numbers/0165-compare-version-numbers.java
@@ -1,16 +1,23 @@
-public class Solution {
+class Solution {
     public int compareVersion(String version1, String version2) {
-        String[] v1 = version1.split("\\.");
-        String[] v2 = version2.split("\\.");
+        String[] parts1 = version1.split("\\.");
+        String[] parts2 = version2.split("\\.");
         
-        int len = Math.max(v1.length, v2.length);
+        int n1 = parts1.length;
+        int n2 = parts2.length;
+        int n = Math.max(n1, n2);
         
-        for (int i = 0; i < len; i++) {
-            int num1 = i < v1.length ? Integer.parseInt(v1[i]) : 0;
-            int num2 = i < v2.length ? Integer.parseInt(v2[i]) : 0;
+        for (int i = 0; i < n; i++) {
+            // parse each segment; if one string is shorter, treat missing parts as 0
+            int v1 = i < n1 ? Integer.parseInt(parts1[i]) : 0;
+            int v2 = i < n2 ? Integer.parseInt(parts2[i]) : 0;
             
-            if (num1 > num2) return 1;
-            if (num1 < num2) return -1;
+            if (v1 < v2) {
+                return -1;
+            } else if (v1 > v2) {
+                return 1;
+            }
+            // else, continue
         }
         
         return 0;
