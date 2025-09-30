c837554d9903b6cad5e336722c80a0c70b7135b8
Sima Pandit <160927044+S-i-m-a@users.noreply.github.com>
2025-09-30 09:01:43 +0530

    Time: 42 ms (86.32%), Space: 44.2 MB (94.42%) - LeetHub

--- 2324-find-triangular-sum-of-an-array/2324-find-triangular-sum-of-an-array.java
+++ 2324-find-triangular-sum-of-an-array/2324-find-triangular-sum-of-an-array.java
@@ -1,15 +1,12 @@
 class Solution {
     public int triangularSum(int[] nums) {
-        // Repeat until only one element remains in the array
-        while (nums.length > 1) {
-            for (int i = 0; i < nums.length - 1; i++) {
-                // Update nums[i] with the sum of nums[i] and nums[i+1] modulo 10
+        int n = nums.length;
+        // We repeatedly reduce the array length by 1 until we have one element
+        for (int len = n; len > 1; len--) {
+            for (int i = 0; i < len - 1; i++) {
                 nums[i] = (nums[i] + nums[i + 1]) % 10;
             }
-            // Shorten the array (by one element)
-            nums = Arrays.copyOf(nums, nums.length - 1);
         }
-        // The final element is the triangular sum
         return nums[0];
     }
 }
