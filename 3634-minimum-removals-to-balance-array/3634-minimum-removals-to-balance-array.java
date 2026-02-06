642aacd5fa7657cf2e98f376ff972c578c3f81ce
Sima Pandit <160927044+S-i-m-a@users.noreply.github.com>
2026-02-06 08:32:16 +0530

    Time: 24 ms (93.17%), Space: 85.8 MB (95.18%) - LeetHub

---
+++ 3634-minimum-removals-to-balance-array/3634-minimum-removals-to-balance-array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minRemoval(int[] nums, int k) {
+        int n = nums.length;
+        Arrays.sort(nums);
+
+        int left = 0;
+        int maxKeep = 0;
+
+        for (int right = 0; right < n; right++) {
+            while (left <= right && (long) nums[right] > (long) nums[left] * k) {
+                left++;
+            }
+            maxKeep = Math.max(maxKeep, right - left + 1);
+        }
+
+        return n - maxKeep;
+    }
+}
