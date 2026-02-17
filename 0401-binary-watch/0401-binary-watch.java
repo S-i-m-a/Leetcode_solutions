10767264939389461e2e4d889cac54474a272c6c
Sima Pandit <160927044+S-i-m-a@users.noreply.github.com>
2026-02-17 09:06:25 +0530

    Time: 4 ms (89.09%), Space: 49.1 MB (55.26%) - LeetHub

--- 0401-binary-watch/0401-binary-watch.java
+++ 0401-binary-watch/0401-binary-watch.java
@@ -1,33 +1,24 @@
-import java.util.ArrayList;
-import java.util.List;
+class Solution {
+  public List<String> readBinaryWatch(int turnedOn) {
+    List<String> ans = new ArrayList<>();
+    dfs(turnedOn, 0, 0, 0, ans);
+    return ans;
+  }
 
-public class Solution {
-    public List<String> readBinaryWatch(int num) {
-        List<String> result = new ArrayList<>();
-        
-        // Iterate over all possible hour values (0 to 11) and minute values (0 to 59)
-        for (int hour = 0; hour < 12; hour++) {
-            for (int minute = 0; minute < 60; minute++) {
-                // Count the number of 1 bits in the binary representation of hour + minute
-                if (Integer.bitCount(hour) + Integer.bitCount(minute) == num) {
-                    // Format the time as "h:mm"
-                    result.add(hour + ":" + (minute < 10 ? "0" + minute : minute));
-                }
-            }
-        }
-        
-        return result;
+  private int[] hours = new int[] {1, 2, 4, 8};
+  private int[] minutes = new int[] {1, 2, 4, 8, 16, 32};
+
+  private void dfs(int turnedOn, int s, int h, int m, List<String> ans) {
+    if (turnedOn == 0) {
+      final String time = String.valueOf(h) + ":" + (m < 10 ? "0" : "") + String.valueOf(m);
+      ans.add(time);
+      return;
     }
 
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        
-        // Test case 1: num = 1 (1 LED on)
-        List<String> result1 = solution.readBinaryWatch(1);
-        System.out.println(result1);  // Expected output: ["1:00", "2:00", "4:00", "8:00", "0:01", "0:02", "0:04", "0:08", "0:16", "0:32"]
-        
-        // Test case 2: num = 2 (2 LEDs on)
-        List<String> result2 = solution.readBinaryWatch(2);
-        System.out.println(result2);  // Output will contain all valid times with exactly 2 LEDs on
-    }
+    for (int i = s; i < hours.length + minutes.length; ++i)
+      if (i < 4 && h + hours[i] < 12)
+        dfs(turnedOn - 1, i + 1, h + hours[i], m, ans);
+      else if (i >= 4 && m + minutes[i - 4] < 60)
+        dfs(turnedOn - 1, i + 1, h, m + minutes[i - 4], ans);
+  }
 }
